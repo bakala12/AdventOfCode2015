93824326899d414d15e4f1eede3872e69e017ca1
Mateusz Bąkała <maba@netcompany.com>
2022-03-10 11:17:11 +0100

    Day 1

--- AocDay.cs
+++ AocDay.cs
@@ -11,15 +11,15 @@ public abstract class AocDay<TParsedInput> : IDay
         _inputParser = inputParser;
     }
 
-    protected abstract int Year { get; }
+    protected virtual string InputFilePath => $"InputFiles/{GetType().Name}.txt";
 
     protected abstract void Part1(TParsedInput input);
     protected abstract void Part2(TParsedInput input);
 
     public void Solve(string[] args)
     {
-        //var fileContent = File.ReadAllText(inputFilePath);
-        var input = _inputParser.ParseInput(string.Empty);
+        var fileContent = File.ReadAllText(InputFilePath);
+        var input = _inputParser.ParseInput(fileContent);
         Part1(input);
         Part2(input);
     }
--- Days/Day1.cs
+++ Days/Day1.cs
@@ -2,7 +2,7 @@ using AdventOfCode2015.Input;
 
 namespace AdventOfCode2015.Days
 {
-    public class Day1 : Aoc2015Day<string>
+    public class Day1 : AocDay<string>
     {
         public Day1(IInputParser<string> inputParser) : base(inputParser)
         {
@@ -10,12 +10,23 @@ namespace AdventOfCode2015.Days
 
         protected override void Part1(string input)
         {
-            Console.WriteLine("Day 1 - part 1");
+            int f = 0;
+            foreach(var c in input)
+                f += c switch { '(' => 1, ')' => -1, _ => 0 };
+            Console.WriteLine(f);
         }
 
         protected override void Part2(string input)
         {
-            Console.WriteLine("Day 1 - part 2");
+            int f = 0;
+            int i = 0;
+            for(; i < input.Length; i++)
+            {
+                f += input[i] switch { '(' => 1, ')' => -1, _ => 0 };
+                if(f < 0)
+                    break;
+            }
+            Console.WriteLine(i+1);
         }
     }
 }
